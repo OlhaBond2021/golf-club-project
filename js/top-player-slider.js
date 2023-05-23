dc92a906fe38c4b2e67d76e6fd78b8ac81e2afb0
OlhaBond2021 <96439245+OlhaBond2021@users.noreply.github.com>
2023-05-23 10:45:11 +0300

    add slider styles

---
+++ js/top-player-slider.js
@@ -0,0 +1,26 @@
+$(document).ready(function () {
+  $('.slider').slick({
+    dots: false,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    // autoplay: true,
+    // autoplaySpeed: 3000,
+    lazyLoad: 'ondemand',
+    responsive: [
+      {
+        breakpoint: 800,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
+  });
+});
